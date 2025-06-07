5d723db018e81aab5cbe53d06f8afdfd4471aa7a
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-06-08 00:50:00 +0530

    deleted backend API

--- api/app/dependencies.py
+++
@@ -1,6 +0,0 @@
-from .services.digipin_services import DigipinService
-
-
-def get_digipin_service() -> DigipinService:
-    """Dependency for DigipinService."""
-    return DigipinService()
--- api/app/routes/v1/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .endpoints.digipin import router as digipin_router
-
-routers = APIRouter()
-router_list = [digipin_router]
-
-for router in router_list:
-    routers.include_router(router, tags=["v1"])
